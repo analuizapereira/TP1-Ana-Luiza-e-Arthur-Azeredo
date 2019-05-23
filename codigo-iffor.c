866a819c30896946021dd68ae93c5147b8ab1a14
analuizapereira <analuizapereira877@gmail.com>
2019-05-23 10:51:20 -0300

    codigo em c

---
+++ codigo-iffor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+       int main (){
+                 int a, b, c, d;
+                 printf(“Digite dois numeros inteiros:\n”);
+                 scanf(“%d %d” ,&a, &b);
+                 if(a>b)}
+                          c=a
+                    d=a;
+         }
+        for(){
+        }
+        return 0;
+   }
+
